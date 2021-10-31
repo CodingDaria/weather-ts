afe78bf2058ddc075034cc970ec8b896b2e47ca6
Daria <61702766+CodingDaria@users.noreply.github.com>
2021-10-31 21:22:01 +0200

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import './assets/css/index.css';
+
+import reportWebVitals from './reportWebVitals';
+import { getStore } from './store';
+
+import App from './App';
+
+const { store, persistor } = getStore();
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
